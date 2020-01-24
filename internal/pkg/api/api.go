6adeafbda4a90bbdaa42436d7f0661b83d842204
Max Curran <max.curran@limejump.com>
2020-01-24 16:25:56 +0100

    Construct URI and call exchangeratesapi.io
    Programmatically construct the URI used to call exchangeratesapi.io.
    This allows a query parameter - currency - that can be used to specify
    which currency should be analysed. Also ensure that data is retrieved
    from the last week from runtime.
    
    Take the response and unmarshal into a map[string]interface ready for
    further processing.

--- internal/pkg/api/api.go
+++ internal/pkg/api/api.go
@@ -2,9 +2,12 @@ package api
 
 import (
 	"encoding/json"
+	"fmt"
+	"io/ioutil"
 	"log"
 	"net/http"
 	"os"
+	"time"
 
 	"github.com/julienschmidt/httprouter"
 )
@@ -27,6 +30,7 @@ func NewServer() *Server {
 
 func (s *Server) buildRouteTable() {
 	s.Router.GET("/api/probe", s.handleProbe())
+	s.Router.GET("/api/rate", s.handleRate())
 }
 
 // encodeJSON takes content of any type (v) and encodes to the writer (w) in
@@ -54,4 +58,39 @@ func (s *Server) handleProbe() httprouter.Handle {
 		payload := response{Message: "Application healthy!"}
 		s.encodeJSON(w, payload, s.logger)
 	}
+}
+
+// handleRate fetches the latest value of £1 and $1 in €s. It also makes a
+// recommendation whether or not it's worthwhile to exchange a currency at the
+// current time based on historical data from the previous week.
+func (s *Server) handleRate() httprouter.Handle {
+	return func(w http.ResponseWriter, r *http.Request, _ httprouter.Params) {
+		params := r.URL.Query()
+		s.logger.Printf("Calling exchangeratesapi.io/history\n")
+		res, err := http.Get(buildURI(params.Get("currency")))
+		if err != nil {
+			Error(w, err, http.StatusServiceUnavailable)
+		}
+
+		body, err := ioutil.ReadAll(res.Body)
+		if err != nil {
+			Error(w, err, http.StatusUnprocessableEntity)
+		}
+
+		var payload map[string]interface{}
+		err = json.Unmarshal(body, &payload)
+		if err != nil {
+			Error(w, err, http.StatusUnprocessableEntity)
+		}
+
+		s.encodeJSON(w, payload, s.logger)
+	}
+}
+
+func buildURI(currency string) string {
+	date := "2006-01-02"
+	today := time.Now().Format(date)
+	lastWeek := time.Now().AddDate(0, 0, -7).Format(date)
+	uri := fmt.Sprintf("https://api.exchangeratesapi.io/history?start_at=%s&end_at=%s&symbols=%s", lastWeek, today, currency)
+	return uri
 }
