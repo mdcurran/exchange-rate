383bd01a1f57738e2047ae99191ccdba6a90d4ba
Max Curran <max.curran@limejump.com>
2020-01-24 15:33:54 +0100

    Define k8s probe and custom errors
    handleProbe() will act as the Kubernetes liveness and readiness probes.
    Implemented custom errors that will encode the error message and HTTP
    status code in a JSON response.

--- internal/pkg/api/api.go
+++ internal/pkg/api/api.go
@@ -1,7 +1,9 @@
 package api
 
 import (
+	"encoding/json"
 	"log"
+	"net/http"
 	"os"
 
 	"github.com/julienschmidt/httprouter"
@@ -13,10 +15,43 @@ type Server struct {
 	logger *log.Logger
 }
 
+// NewServer instantiates an HTTP server and builds a route table.
 func NewServer() *Server {
 	s := &Server{
 		Router: httprouter.New(),
 		logger: log.New(os.Stderr, "API: ", 0),
 	}
+	s.buildRouteTable()
 	return s
+}
+
+func (s *Server) buildRouteTable() {
+	s.Router.GET("/api/probe", s.handleProbe())
+}
+
+// encodeJSON takes content of any type (v) and encodes to the writer (w) in
+// JSON format.
+func (s *Server) encodeJSON(w http.ResponseWriter, v interface{}, l *log.Logger) {
+	w.Header().Set("Content-Type", "application/json")
+	err := json.NewEncoder(w).Encode(v)
+	if err != nil {
+		Error(w, err, http.StatusInternalServerError)
+	}
+}
+
+// handleProbe acts as the Kubernetes liveness & readiness probes.
+//
+// In most scenarios, liveness & readiness probes should probably do different
+// things. For example, a readiness probe might include making a call to a
+// database to ensure that functionality works. However for this exercise
+// simply being able to get a 200 from an endpoint should suffice for both
+// probes.
+func (s *Server) handleProbe() httprouter.Handle {
+	type response struct {
+		Message string `json:"message"`
+	}
+	return func(w http.ResponseWriter, r *http.Request, _ httprouter.Params) {
+		payload := response{Message: "Application healthy!"}
+		s.encodeJSON(w, payload, s.logger)
+	}
 }
